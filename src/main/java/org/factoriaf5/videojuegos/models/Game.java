4bfc09c78d39c917b4babe1b245034b1817ebb88
anagarcialainez <accgl@hotmail.com>
2021-06-10 10:50:32 +0200

    corrigiendo archivos data/schema)

--- src/main/java/org/factoriaf5/videojuegos/models/Game.java
+++ src/main/java/org/factoriaf5/videojuegos/models/Game.java
@@ -17,14 +17,7 @@ import static javax.persistence.GenerationType.SEQUENCE;
         private String title;
         private String platform;
         private int years;
-        private double priceOriginal;
-        private String etiqueta;
-        private String discountPercent;
-        private double priceDiscount;
-        private String category;
-        private String publisher;
-        private int pegi;
-        private String pegiContentDescriptiors;
+
 
         public Long getId() {
             return id;
@@ -58,69 +51,7 @@ import static javax.persistence.GenerationType.SEQUENCE;
             this.years = years;
         }
 
-        public double getPriceOriginal() {
-            return priceOriginal;
-        }
-
-        public void setPriceOriginal(double priceOriginal) {
-            this.priceOriginal = priceOriginal;
-        }
-
-        public String getEtiqueta() {
-            return etiqueta;
-        }
-
-        public void setEtiqueta(String etiqueta) {
-            this.etiqueta = etiqueta;
-        }
-
-        public String getDiscountPercent() {
-            return discountPercent;
-        }
-
-        public void setDiscountPercent(String discountPercent) {
-            this.discountPercent = discountPercent;
-        }
-
-        public double getPriceDiscount() {
-            return priceDiscount;
-        }
-
-        public void setPriceDiscount(double priceDiscount) {
-            this.priceDiscount = priceDiscount;
-        }
-
-        public String getCategory() {
-            return category;
-        }
-
-        public void setCategory(String category) {
-            this.category = category;
-        }
-
-        public String getPublisher() {
-            return publisher;
-        }
-
-        public void setPublisher(String publisher) {
-            this.publisher = publisher;
-        }
-
-        public int getPegi() {
-            return pegi;
-        }
-
-        public void setPegi(int pegi) {
-            this.pegi = pegi;
-        }
-
-        public String getPegiContentDescriptiors() {
-            return pegiContentDescriptiors;
-        }
-
-        public void setPegiContentDescriptiors(String pegiContentDescriptiors) {
-            this.pegiContentDescriptiors = pegiContentDescriptiors;}
-        }
+    }
 
 
 
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO games(title, Platform, years ,priceOriginal)
+INSERT INTO games(title, platform, years)
 VALUES ('Wii Sports', 'Wii', '2006'),
        ('Super Mario Bros.', 'NES', '1985'),
        ('Mario Kart Wii', 'Wii', '2008');
