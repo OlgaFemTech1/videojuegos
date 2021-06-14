53b1226b2c53519a62d1b7ebf8c4d20e152d98b8
Olga Caparros <caparrosolga@gmail.com>
2021-06-14 11:53:55 +0200

    GameController añadiendo

--- src/main/java/org/factoriaf5/videojuegos/controllers/GameController.java
+++ src/main/java/org/factoriaf5/videojuegos/controllers/GameController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
@@ -25,5 +27,16 @@ public class GameController {
         model.addAttribute("title", "Games list");
         model.addAttribute("games", games);
         return "games/all";
+    }
+    @GetMapping("/games/new")
+    String getForm(Model model) {
+        Game game = new Game();
+        model.addAttribute("game", game);
+        return "games/new";
+    }
+    @PostMapping("/games/new")
+    String addGame(@ModelAttribute Game game) {
+        gameService.save(game);
+        return "redirect:/games";
     }
 }
