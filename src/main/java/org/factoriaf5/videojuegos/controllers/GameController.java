8d1ef7da686d8a9def1af5540c94519ed0aeda53
Olga Caparros <caparrosolga@gmail.com>
2021-06-15 11:30:35 +0200

    maping new y delete

--- src/main/java/org/factoriaf5/videojuegos/controllers/GameController.java
+++ src/main/java/org/factoriaf5/videojuegos/controllers/GameController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
@@ -39,4 +40,19 @@ public class GameController {
         gameService.save(game);
         return "redirect:/games";
     }
+  @GetMapping("/edit/{id}")
+    String editGame(Model model, @PathVariable Long id){
+        Game game = gameService.findById(id);
+       model.addAttribute("game", game);
+        model.addAttribute("title", "Edit game");
+       return "games/new";
+    }
+
+    @GetMapping("/delete/{id}")
+    String removeGame(@PathVariable Long id){
+        gameService.delete(id);
+        return "redirect:/games";
+   }
+
+
 }
