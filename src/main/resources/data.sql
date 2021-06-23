74aa1efaa09a8817c58b9f3978a38f0808f93d64
anagarcialainez <accgl@hotmail.com>
2021-06-23 09:29:29 +0200

    introduciendo mas juego ala bd

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,8 +1,8 @@
 INSERT INTO games(title, platform, years, pegi, cost, etiqueta, sales, photo)
-VALUES ('Wii Sports', 'Wii', '2006', '7', '19.99', 'default', '17.99', '/img/1 wii-sports-retro-portada.jpg'),
-       ('Super Mario Bros', 'NES', '1985', '7', '14.99', 'default', '17.99', 'default'),
-       ('Mario Kart Wii', 'Wii', '2008', '7', '9.99', 'default', '17.99', 'default'),
-       ('Wii Play', 'Wii', '2006', '7', '6.99', 'pack2', '6.99', 'default'),
-       ('Super Mario Bros', 'NES', '1985', '7', '14.99', 'default', '17.99', 'default'),
-       ('Super Mario Bros', 'NES', '1985', '7', '14.99', 'default', '17.99', 'default'),
-       ('Super Mario Bros', 'NES', '1985', '7', '14.99', 'default', '17.99', 'default');
+VALUES ('Wii Sports', 'Wii', '2006', '7', '19.99', 'default', '0.00', 'default'),
+       ('Super Mario Bros', 'NES', '1985', '7', '14.99', 'default', '0.00', 'default'),
+       ('Mario Kart Wii', 'Wii', '2008', '7', '9.99', 'default', '0.00', 'default'),
+       ('Wii Play', 'Wii', '2006', '7', '6.99', 'PACK2', '6.99', 'default'),
+       ('Grand Theft Auto: San Andreas', 'PS2', '2004', '18', '24.99', 'SUPER', '22.49', 'default'),
+       ('Call of Duty: Black Ops II', 'X360', '2012', '16', '29.99', 'MEGA', '23.99', 'default'),
+       ('Pokemon Red/Pokemon Blue', 'GB', '1996', '7', '29.99', 'default', '0.00', 'default');
